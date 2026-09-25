7458ea43bbd8eb241ac37493fb5f82f851c4f2ec
legen <legenadary.pigeon@gmail.com>
2026-09-26 01:31:46 +0300

    get user endpoint, mapping updated

--- internal/user/handler/response.go
+++ internal/user/handler/response.go
@@ -5,3 +5,7 @@ import "Test2/internal/user/model"
 type UsersListResponse struct {
 	Users []model.User
 }
+
+type UserResponse struct {
+	User model.User
+}
--- internal/user/model/user.go
+++ internal/user/model/user.go
@@ -1,6 +1,8 @@
 package model
 
 type User struct {
-	ID   int64
-	Name string
+	ID      int64
+	Name    string
+	TgID    int64
+	PartyID *int
 }
